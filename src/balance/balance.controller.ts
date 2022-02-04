e6f6d8a86da0d2d4a61197cb4ff4db13dc582d7d
tiger1016 <applejuicy1016@gmail.com>
2022-02-04 11:50:26 -0500

    Add reset method

--- src/balance/balance.controller.ts
+++ src/balance/balance.controller.ts
@@ -1,4 +1,5 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('balance')
-export class BalanceController {}
+export class BalanceController {
+}
